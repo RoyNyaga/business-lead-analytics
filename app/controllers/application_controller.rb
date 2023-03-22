33e333ebd8ae5dccbe536a01b97298d43b415b76
Andre <andre@vdbapp.com>
2023-03-22 15:07:56 +0100

    add after_sign_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def after_sign_in_path_for(resource)
+    if resource.businesses.size == 0
+      stored_location_for(resource) || new_business_path
+    else
+      stored_location_for(resource) || business_path
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :title])
   end
