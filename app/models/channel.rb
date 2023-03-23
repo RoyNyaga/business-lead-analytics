529bd4a3708eadf88425071cb31df09ab12fc867
Andre <andre@vdbapp.com>
2023-03-23 13:53:20 +0100

    add validation to channel name

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,4 +1,8 @@
 class Channel < ApplicationRecord
   belongs_to :business
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: { scope: :business_id, 
+    message: ": Channel already Exist." }
+
 end
