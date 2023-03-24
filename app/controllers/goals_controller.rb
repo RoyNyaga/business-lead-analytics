76c5a463bc4bc8862e904f91a99e6bf84e171688
Andre <andre@vdbapp.com>
2023-03-24 12:27:16 +0100

    adjust goal form

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,21 @@
 class GoalsController < ApplicationController
 
-  def create 
+  def create
+    @goal = Goal.new(goal_params)
+    @business = Business.find_by(id: params[:goal][:business_id])
+    respond_to do |format|
+      if @goal.save
+        format.html { redirect_to business_path(@business), notice: "Goal was successfully Created" }
+      else
+        format.html { redirect_to business_path(@business), status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private 
+
+  def goal_params
+    params[:goal][:channels] = params[:goal][:channels].join(" -#- ")
+    params.require(:goal).permit(:quater_name, :projected_leads, :budget, :projected_conversion_rate, :projected_revenue, :channels, :business_id, :user_id)
   end
 end
