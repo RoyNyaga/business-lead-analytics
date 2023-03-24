64839b4d2c8101702af303a8915db28a1c471a80
Andre <andre@vdbapp.com>
2023-03-24 13:17:18 +0100

    create goal card

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,6 +2,7 @@ class Business < ApplicationRecord
   belongs_to :industry
   belongs_to :user
   has_many :channels
+  has_many :goals
 
   validates :name, presence: true
   validates :products, presence: true
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,5 @@
 class Goal < ApplicationRecord
+  belongs_to :business
+  
   QUATER_OPTIONS = ["Q1", "Q2", "Q3", "Q4"]
 end
