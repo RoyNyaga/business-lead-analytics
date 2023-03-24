b6d51b743aab74fd9785230ace69be9dc328dd2a
Andre <andre@vdbapp.com>
2023-03-24 20:17:23 +0100

    create product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_000103) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_191700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_000103) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.bigint "business_id", null: false
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["business_id"], name: "index_products_on_business_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,4 +89,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_000103) do
   add_foreign_key "channels", "users"
   add_foreign_key "goals", "businesses"
   add_foreign_key "goals", "users"
+  add_foreign_key "products", "businesses"
+  add_foreign_key "products", "users"
 end
