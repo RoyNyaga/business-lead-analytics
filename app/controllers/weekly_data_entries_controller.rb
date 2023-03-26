5411fef8d75bfd1cee02bfeeb8b23fb83ea27f6f
Andre <andre@vdbapp.com>
2023-03-26 15:41:34 +0100

    update weekly_data_entries form

--- app/controllers/weekly_data_entries_controller.rb
+++ app/controllers/weekly_data_entries_controller.rb
@@ -7,6 +7,6 @@ class WeeklyDataEntriesController < ApplicationController
   private 
 
   def weekly_data_entry_params
-    params.require(:weekly_data_entry).permit(:goal_id, :business_id, :user_id, :product_id, :contacted_leads, :paid_customers, :budget_expences, :revenue, :conversion_rate)
+    params.require(:weekly_data_entry).permit(:goal_id, :business_id, :user_id, :product_id, :contacted_leads, :paid_customers, :budget_expences, :revenue, :conversion_rate, :date)
   end
 end
