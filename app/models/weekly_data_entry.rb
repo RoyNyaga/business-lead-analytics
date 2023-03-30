1a3d5a1775100437e051942235b63f67e85814be
Andre <andre@vdbapp.com>
2023-03-30 22:27:04 +0100

    style goal show page

--- app/models/weekly_data_entry.rb
+++ app/models/weekly_data_entry.rb
@@ -8,7 +8,7 @@ class WeeklyDataEntry < ApplicationRecord
   def parse_channel_leads(params)
     leads_per_channel = []
     business.channels.each do |channel|
-      leads_per_channel << { "#{channel.name}" => params["#{channel.name}"] }
+      leads_per_channel << { "#{channel.name}" => params["#{channel.name}"] } if goal.channel_arr.include?(channel.name)
     end
 
     self.channel_leads_per_week = leads_per_channel
@@ -26,7 +26,7 @@ class WeeklyDataEntry < ApplicationRecord
   def parse_product_leads(params)
     leads_per_product = []
     business.products.each do |product|
-      leads_per_product << { "#{product.name}" => params["#{product.name}"] }
+      leads_per_product << { "#{product.name}" => params["#{product.name}"] } if goal.product_arr.include?(product.name)
     end
 
     self.product_leads_per_week = leads_per_product
@@ -43,5 +43,13 @@ class WeeklyDataEntry < ApplicationRecord
 
   def set_conversion_rate
     self.conversion_rate = calc_conversion_rate
+  end
+
+  def channel_leads_array_of_hashes
+    channel_leads_per_week.map{ |channel| eval(channel) }
+  end
+
+  def product_leads_array_of_hashes
+    product_leads_per_week.map{ |product| eval(product) }
   end
 end
