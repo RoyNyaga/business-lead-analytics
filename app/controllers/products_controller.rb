7e72882474fc44f7506e1b5a407a7358034d0c2b
Andre <andre@vdbapp.com>
2023-04-04 11:59:29 +0100

    add product update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:update]
+  
   def create
     @product = Product.new(product_params)
     @business = Business.find_by(id: params[:product][:business_id])
@@ -12,7 +14,27 @@ class ProductsController < ApplicationController
     end
   end
 
+  def update
+    @business = @product.business
+
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to business_path(@business), notice: "Product was successfully updated" }
+      else
+        format.html { redirect_to business_path(@business), alart: "Error!!, Product could not be updated: #{@product.errors.full_message.join(",")}" }
+      end
+    end
+  end
+
+  def set_product
+    @product = Product.find_by(id: params[:id])
+  end
+
   private 
+
+  def set_product
+    @product = Product.find_by(id: params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:user_id, :business_id, :name)
